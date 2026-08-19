f3908b176913e96d88116896713f992a9b7efaa1
DEK593 <dek.keed023@gmail.com>
2026-08-20 00:51:21 +0200

    Add single-user ticket price script with input validation

--- 004javascript.js
+++ 004javascript.js
@@ -3,16 +3,17 @@ import { stdin as input, stdout as output } from "node:process";
 
 const rl = readline.createInterface({ input, output });
 
-const età = await rl.question("quanti anni hai?");
-
+const età = await rl.question("Quanti anni hai? ");
 let anni = parseInt(età, 10);
 
-if (anni <= 12) {
-  console.log("biglietto ridotto 5 euro");
+if (isNaN(anni)) {
+  console.log("Errore: inserisci un numero valido!");
+} else if (anni <= 12) {
+  console.log("Biglietto ridotto 5 euro");
 } else if (anni >= 65) {
-  console.log("biglietto ridotto 6 euro");
+  console.log("Biglietto ridotto 6 euro");
 } else {
-  console.log("biglietto intero 10 euro");
+  console.log("Biglietto intero 10 euro");
 }
 
 rl.close();
