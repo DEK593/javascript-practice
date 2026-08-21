de78e636fffb679685e7fc715c169380c221a432
DEK593 <dek.keed023@gmail.com>
2026-08-22 01:38:19 +0200

    fix: use Number() for strict input validation and clean up firewall logic

--- 007javascript.js
+++ 007javascript.js
@@ -1,6 +1,6 @@
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
-
+import { setTimeout } from "node:timers/promises";
 const rl = readline.createInterface({ input, output });
 let max_totaleping = 15;
 let totale_ping = 0;
@@ -9,14 +9,17 @@ while (totale_ping < max_totaleping) {
   const text = await rl.question(
     "How many pings do you want to send (max 5 at round)?",
   );
-  let number = parseInt(text, 10);
-  if (isNaN(number)) {
+  let number = Number(text);
+  if (isNaN(number) || number <= 0) {
     console.log("is not a number");
     continue;
   }
 
   if (totale_ping + number > max_totaleping || number > 5) {
     console.log("active firewall");
+    console.log("timeout");
+    await setTimeout(30000);
+
     break;
   }
   totale_ping = totale_ping + number;
